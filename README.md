e6ed11635f5820f89e69e0d61d608fd321c721e6
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-05-10 10:18:04 -0300

    Initial Setup

---
+++ README.md
@@ -0,0 +1,25 @@
+## The app
+
+In construction...
+
+## How to use
+
+```
+# On your terminal
+
+git clone git://github.com/pedrogglima/frontend-tracking-time-app
+
+# Then install the dependencies
+
+npm install
+
+# After
+
+...
+
+# After starting the server, you can find the app on ...
+```
+
+## Licenses and Copyright
+
+Copyright (C) 2019 Pedro Gabriel Lima.  
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ]
+      }
+    ]
+  },
+};
