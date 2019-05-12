c3b0b9eb71dea9091145f8c0cbefe452f0fdcb96
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-05-12 11:47:47 -0300

    added style to component Timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 class Timer extends React.Component {
   render() {
@@ -8,10 +11,25 @@ class Timer extends React.Component {
     return (
       <Card>
         <CardContent>
-          {this.props.title}
-          {this.props.project}
-          {elapsedString}
-          Start
+          <Typography variant='title' color="primary">
+            {this.props.title}
+          </Typography>
+          <Typography color="textSecondary" gutterBottom>
+            {this.props.project}
+          </Typography>
+          <Typography variant='h3' align='center' color="default" gutterBottom>
+            {elapsedString}02:20:30
+          </Typography>
+
+          <IconButton aria-label="Delete" style={{ float: 'right' }}>
+            <span className="material-icons">delete</span>
+          </IconButton>
+          <IconButton aria-label="Edit" style={{ float: 'right' }}>
+            <span className="material-icons">edit</span>
+          </IconButton>
+          <Button variant="contained" size="large" fullWidth={true}>
+            Start
+          </Button>
         </CardContent>
       </Card>
     );
